2c4ab879d126e5e9c7e420f660a9c5225de0cefb
lavandos228 <vlad.babich.vlad@mail.ru>
2021-05-07 18:21:10 +0300

    final project

--- app/src/main/java/me/lavandos/traning/MainActivity.kt
+++ app/src/main/java/me/lavandos/traning/MainActivity.kt
@@ -1,11 +1,27 @@
 package me.lavandos.traning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //метод для кнопок
+        onClick()
+
+    }
+
+    private fun onClick() {
+        //это команда для вызова действия с кнопкой
+        button1.setOnClickListener { newPage() }
+    }
+
+    private fun newPage() {
+        //это команда показывает куда нас перенаправит при нажатии
+        val intent = Intent(this,listAuto_activity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/me/lavandos/traning/listAuto_activity.kt
@@ -0,0 +1,11 @@
+package me.lavandos.traning
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class listAuto_activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_auto_activity)
+    }
+}
